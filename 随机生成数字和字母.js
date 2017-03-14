51699b2dd7a2f6cdc3785235e88afad7b8cb4fe3
Kell-Wang <2416451355@qq.com>
2017-03-14 11:31:01 +0800

    随机生成数字和字母

--- 随机生成数字和字母.js
+++ 随机生成数字和字母.js
@@ -1,3 +1,4 @@
+/** Create on 2017/3/13*/
 
 function randomString(length) {
     // split() : 基于指定的分隔符将一个字符串分割成多个子字符串，并将结果放在一个数组中。
@@ -18,3 +19,17 @@ console.log(str + ",");
 for(var i=0; i<100; i++){
     randomString(6);
 }
+
+
+
+
+/*
+console.log(Math.random()); //0.9395614803309535
+console.log(Math.floor(Math.random() * 10));  //1
+
+var arr = [];
+for(var i=0; i<20; i++){
+    //console.log(Math.floor(Math.random() * 10));
+    arr.push(Math.floor(Math.random() * 10));
+}
+console.log(arr);  //[ 2, 0, 7, 4, 3, 5, 3, 5, 0, 4, 8, 1, 1, 6, 4, 4, 4, 5, 9, 8 ] */
